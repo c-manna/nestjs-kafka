762119d1099addecb58593fdd794ad115376328d
Chandra <chandrakanta@onebillionideas.io>
2026-02-15 08:04:33 +0530

    update

--- apps/kafka-consumer/src/main.ts
+++ apps/kafka-consumer/src/main.ts
@@ -2,32 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { KafkaConsumerModule } from './kafka-consumer.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-// async function bootstrap() {
-
-//   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-//     KafkaConsumerModule,
-//     {
-//       transport: Transport.KAFKA,
-//       options: {
-//         client: {
-//           clientId: 'nestjs-server',
-//           brokers: ['localhost:9092'], // host run
-//         },
-//         consumer: {
-//           groupId: 'nestjs-server-v1',
-//           sessionTimeout: 30000,
-//           heartbeatInterval: 3000,
-//         },
-//         run: {
-//           autoCommit: true,
-//         },
-//       },
-//     },
-//   );
-
-//   await app.listen();
-// }
-// bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(KafkaConsumerModule);
